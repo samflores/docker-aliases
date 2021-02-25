a31559b4509f55ef9a9878bc2711d0fd4745bb65
Samuel Flores <me@samflor.es>
2021-02-25 20:17:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# docker-aliases
+
+Consult [source file](docker-aliases.plugin.zsh) for more details.
+
+
+## Installation
+
+### [zplug](https://github.com/zplug/zplug)
+
+```zsh
+zplug "samflores/docker-aliases", use:docker-aliases.plugin.zsh
+```
+
+### [Antigen](https://github.com/zsh-users/antigen)
+
+```zsh
+antigen bundle samflores/docker-aliases.git
+```
+
+### Manual
+
+Download the latest released `docker-aliases.plugin.zsh` to `$ZSH/plugins/docker-aliases`: 
+```shell
+curl --create-dirs -o $ZSH/plugins/docker-aliases/docker-aliases.plugin.zsh -sL \
+$(curl -s https://api.github.com/repos/samflores/docker-aliases/releases \
+| grep browser_download_url \
+| head -n 1 \
+| cut -d '"' -f 4)
+```
+
+Add `docker-aliases` to `plugins` in `$ZDOTDIR/.zshrc`.
+
+Restart the shell.
+
+
+## Note
+
+No actions are `-f`orced by default: This is at user's discretion. 
---
+++ docker-aliases.plugin.zsh
@@ -0,0 +1,66 @@
+#        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#                    Version 2, December 2004 
+#
+# Copyright (C) 2021 Samuel Flores <me@samflor.es> 
+#
+# Everyone is permitted to copy and distribute verbatim or modified 
+# copies of this license document, and changing it is allowed as long 
+# as the name is changed. 
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+###########
+# General #
+###########
+
+alias d='docker'
+
+#######################
+# Images - di<action> #
+#######################
+
+alias dil='docker images'
+alias dibu='docker build'
+alias dix='docker rmi'
+alias diX='docker rmi $* $(docker images -a -q)'
+alias dixx='docker rmi $* $(docker images -q -f "dangling=true")'
+alias dih='docker history $*'
+
+###########################
+# Containers - dc<action> #
+###########################
+
+alias dcl='docker ps'
+alias dci='docker inspect --format "{{ .NetworkSettings.IPAddress }}" $*'
+
+alias dcr='docker run -it'
+alias dcex='docker exec -it'
+alias dclg='docker logs'
+
+alias dcs='docker stop $*'
+alias dcS='docker stop $* $(docker ps -q -f "status=running")'
+
+alias dcx='docker rm'
+alias dcxs='docker rm $* $(docker ps -q -f "status=exited")'
+alias dxC='docker rm $* $(docker ps -a -q)'
+
+########################
+# Volumes - dv<action> #
+########################
+
+alias dvl='docker volume ls $*'
+alias dvX='docker volume rm $(docker volume ls -q)'
+alias dvxx='docker volume rm $(docker volume ls -q -f "dangling=true")'
+
+########################
+# Network - dn<action> #
+########################
+
+alias dnl='docker network list'
+alias dnn='docker network create'
+alias dno='docker network connect'
+alias dnx='docker network rm'
+alias dnX='docker network prune'
